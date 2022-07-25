ecaaa15100a8b15d059901df5cbf3846aa08901a
Dmitriy Grape <grapeoff.official@gmail.com>
2022-07-25 14:39:25 +0300

    Add connection retrying and disconnecting on application shutdown

--- src/constants/kindagoose.constants.ts
+++ src/constants/kindagoose.constants.ts
@@ -1,1 +1,2 @@
 export const DEFAULT_CONNECTION_NAME = 'DefaultConnection';
+export const KINDAGOOSE_CONNECTION_NAME = 'KindagooseConnectionName';
--- src/modules/kindagoose-core.module.ts
+++ src/modules/kindagoose-core.module.ts
@@ -1,31 +1,49 @@
-import { DynamicModule, Global, Module, OnApplicationShutdown, Provider } from '@nestjs/common';
+import { DynamicModule, Global, Inject, Logger, Module, OnApplicationShutdown, Provider } from '@nestjs/common';
 import { ModuleRef } from '@nestjs/core';
-import mongoose from 'mongoose';
+import mongoose, { Connection } from 'mongoose';
+import { catchError, delay, from, lastValueFrom, retry } from 'rxjs';
 
+import { KINDAGOOSE_CONNECTION_NAME } from '../constants/kindagoose.constants';
 import { KindagooseModuleOptions } from '../interfaces/kindagoose-module-options.interface';
 import { getConnectionToken } from '../utils/get-connection-token';
 
 @Global()
 @Module({})
 export class KindagooseCoreModule implements OnApplicationShutdown {
-    constructor(private readonly moduleRef: ModuleRef) {}
+    constructor(
+        @Inject(KINDAGOOSE_CONNECTION_NAME) private readonly connectionName: string,
+        private readonly moduleRef: ModuleRef,
+    ) {}
 
     static forRoot(uri: string, options: KindagooseModuleOptions): DynamicModule {
-        const { connectionName, ...mongooseConnectOptions } = options;
+        const { connectionName, retryAttempts, retryDelay, ...mongooseConnectOptions } = options;
 
+        const connectionToken = getConnectionToken(connectionName);
         const connectionProvider: Provider = {
-            provide: getConnectionToken(connectionName),
+            provide: connectionToken,
             async useFactory() {
-                return await mongoose.createConnection(uri, mongooseConnectOptions).asPromise();
+                const logger = new Logger('KindagooseModule');
+
+                return await lastValueFrom(
+                    from(mongoose.createConnection(uri, mongooseConnectOptions).asPromise()).pipe(
+                        catchError((err, caught) => {
+                            logger.error('Caught an error when tried to connect. Retrying...');
+                            return caught.pipe(retry(retryAttempts || 3), delay(retryDelay || 3000));
+                        }),
+                    ),
+                );
             },
         };
 
         return {
             module: KindagooseCoreModule,
-            providers: [connectionProvider],
+            providers: [connectionProvider, { provide: KINDAGOOSE_CONNECTION_NAME, useValue: connectionToken }],
             exports: [connectionProvider],
         };
     }
 
-    onApplicationShutdown(signal?: string): any {}
+    async onApplicationShutdown() {
+        const connection: Connection = this.moduleRef.get(this.connectionName);
+        if (connection) await connection.close();
+    }
 }
