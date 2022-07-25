f67ac190f0f0bbc9b9725995530f8823d3fb0685
Dmitriy Grape <grapeoff.official@gmail.com>
2022-07-25 14:40:19 +0300

    Export all library items

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { InjectConnection } from './decorators/inject-connection.decorator';
+export { InjectModel } from './decorators/inject-model.decorator';
+export { KindagooseModuleOptions } from './interfaces/kindagoose-module-options.interface';
+export { KindagooseModule } from './modules/kindagoose.module';
+export { getConnectionToken } from './utils/get-connection-token';
