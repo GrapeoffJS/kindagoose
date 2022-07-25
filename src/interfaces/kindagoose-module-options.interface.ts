110fbffda2f11ced2b35d2e92b37a8ad9c9f0cfd
Dmitriy Grape <grapeoff.official@gmail.com>
2022-07-25 14:37:25 +0300

    Add documentation

--- src/interfaces/kindagoose-module-options.interface.ts
+++ src/interfaces/kindagoose-module-options.interface.ts
@@ -1,5 +1,10 @@
 import { ConnectOptions } from 'mongoose';
 
+/***
+ * @property {number} retryAttempts - How many times Kindagoose should try to connect to database if it catches an error.
+ * @property {number} retryDelay - How much time Kindagoose should wait before next attempt.
+ * @property {string} connectionName - Name for mongoose connection. If undefined, default connection name will be used.
+ */
 export interface KindagooseModuleOptions extends ConnectOptions {
     retryAttempts?: number;
     retryDelay?: number;
