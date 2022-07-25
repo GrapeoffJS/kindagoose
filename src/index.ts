a0c5c8a08f4cd22a090492e74a10fdf71c616981
Dmitriy Grape <grapeoff.official@gmail.com>
2022-07-25 15:12:25 +0300

    Export types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export { InjectConnection } from './decorators/inject-connection.decorator';
 export { InjectModel } from './decorators/inject-model.decorator';
 export { KindagooseModuleOptions } from './interfaces/kindagoose-module-options.interface';
+export { ModelRegistrationOptions } from './interfaces/model-registration-options.interface';
+export { TypegooseSchema } from './interfaces/typegoose-schema.interface';
 export { KindagooseModule } from './modules/kindagoose.module';
 export { getConnectionToken } from './utils/get-connection-token';
