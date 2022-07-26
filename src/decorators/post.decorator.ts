bbb930fdc59b0dc33b0c57da78e1eb9f7c63961b
Dmitriy Grape <grapeoff.official@gmail.com>
2022-07-27 01:29:49 +0300

    Add Pre and Post decorators

---
+++ src/decorators/post.decorator.ts
@@ -0,0 +1,5 @@
+import { SetMetadata } from '@nestjs/common';
+
+import { PostEvents } from '../constants/post-events';
+
+export const Post = (eventName: PostEvents) => SetMetadata('post', eventName);
---
+++ src/decorators/pre.decorator.ts
@@ -0,0 +1,5 @@
+import { SetMetadata } from '@nestjs/common';
+
+import { PreEvents } from '../constants/pre-events';
+
+export const Pre = (eventName: PreEvents) => SetMetadata('pre', eventName);
