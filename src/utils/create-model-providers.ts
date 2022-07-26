cdf0497500de3bc28ac339a9715c0f1220020cae
Dmitriy Grape <grapeoff.official@gmail.com>
2022-07-26 20:07:50 +0300

    Fix: Models did not use existing connection

--- src/utils/create-model-providers.ts
+++ src/utils/create-model-providers.ts
@@ -1,25 +1,33 @@
 import { Provider } from '@nestjs/common';
 import { getDiscriminatorModelForClass, getModelForClass } from '@typegoose/typegoose';
+import { Connection } from 'mongoose';
 
 import { ModelRegistrationOptions } from '../interfaces/model-registration-options.interface';
+import { getConnectionToken } from './get-connection-token';
 import { getModelToken } from './get-model-token';
 
 export const createModelProviders = (options: ModelRegistrationOptions[], connectionName?: string): Provider[] => {
     const providers: Provider[] = [];
 
     for (const option of options) {
-        const model = getModelForClass(option.schema);
+        const model = getModelForClass(option.schema, {});
 
         providers.push({
             provide: getModelToken(option.schema.name, connectionName),
-            useValue: model,
+            inject: [getConnectionToken(connectionName)],
+            useFactory(connection: Connection) {
+                return getModelForClass(model, { existingConnection: connection });
+            },
         });
 
         if (option.discriminators) {
             for (const discriminator of option.discriminators) {
                 providers.push({
                     provide: getModelToken(discriminator.name, connectionName),
-                    useValue: getDiscriminatorModelForClass(model, discriminator),
+                    inject: [getConnectionToken(connectionName)],
+                    useFactory: (connection: Connection) => {
+                        return getDiscriminatorModelForClass(model, discriminator, { existingConnection: connection });
+                    },
                 });
             }
         }
