0ee357f8435371f4d5626372bc33ac4af0e6ad65
Dmitriy Grape <grapeoff.official@gmail.com>
2022-07-27 01:26:17 +0300

    Add EventTrackerFor decorator

---
+++ src/decorators/event-tracker-for.decorator.ts
@@ -0,0 +1,11 @@
+import { applyDecorators, Injectable, SetMetadata } from '@nestjs/common';
+
+import { AnyClass } from '../interfaces/typegoose-schema.interface';
+
+/**
+ * Mark a class as an event tracker
+ * @param {AnyClass} schema - Typegoose class that will apply listeners of this tracker
+ * @constructor
+ */
+export const EventTrackerFor = (schema: AnyClass) =>
+    applyDecorators(Injectable, SetMetadata('tracker-for', schema.name));
