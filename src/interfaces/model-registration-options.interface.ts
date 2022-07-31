23e93032adea37f57ccf917343ef7517970b18fe
Dmitriy Grape <grapeoff.official@gmail.com>
2022-08-01 01:31:30 +0300

    Remove tracker field

--- src/interfaces/model-registration-options.interface.ts
+++ src/interfaces/model-registration-options.interface.ts
@@ -3,10 +3,8 @@ import { AnyClass } from './any-class.interface';
 /**
  * @property {AnyClass} schema - Typegoose schema.
  * @property {AnyClass} discriminators - Model discriminators.
- * @property {AnyClass} tracker - Event tracker.
  */
 export interface ModelRegistrationOptions {
     schema: AnyClass;
     discriminators?: AnyClass[];
-    tracker?: AnyClass;
 }
