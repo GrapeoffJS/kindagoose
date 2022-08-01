73d09637fc3bf2b0956b747a76e585f36ea8a8a8
Dmitriy Grape <grapeoff.official@gmail.com>
2022-08-01 21:30:47 +0300

    Add MongoDBConnectionURIBuilder

--- src/utils/mongodb-connection-uri-builder.ts
+++ src/utils/mongodb-connection-uri-builder.ts
@@ -1,1 +1,48 @@
-export class MongoDBConnectionURIBuilder {}
+export class MongoDBConnectionURIBuilder {
+    private protocol = '';
+    private hostname = '';
+    private port: number | string = '';
+    private username = '';
+    private password = '';
+    private databaseName = '';
+    private connectionParams = '';
+
+    public setProtocol(protocol: string) {
+        this.protocol = protocol;
+        return this;
+    }
+
+    public setHostname(hostname: string) {
+        this.hostname = hostname;
+        return this;
+    }
+
+    public setPort(port: string | number) {
+        this.port = ':' + port;
+        return this;
+    }
+
+    public setUsername(username: string) {
+        this.username = username;
+        return this;
+    }
+
+    public setPassword(password: string) {
+        this.password = password + '@';
+        return this;
+    }
+
+    public setDatabaseName(dbName: string) {
+        this.databaseName = dbName;
+        return this;
+    }
+
+    public setConnectionParams(params: string) {
+        this.connectionParams = params;
+        return this;
+    }
+
+    public build() {
+        return `${this.protocol}/${this.username}/${this.password}${this.hostname}${this.port}/${this.databaseName}?${this.connectionParams}`;
+    }
+}
