e6320551dee11db17590b7494ce23b1913bff3e4
Dmitriy Grape <grapeoff.official@gmail.com>
2023-04-10 23:24:48 +0300

    Fix grammar in the JSDoc comment

--- src/interfaces/kindagoose-module-options.interface.ts
+++ src/interfaces/kindagoose-module-options.interface.ts
@@ -1,7 +1,7 @@
 import { ConnectOptions } from 'mongoose';
 
 /***
- * @property {string} connectionName - Name for mongoose connection. If undefined, default connection name will be used.
+ * @property {string} connectionName - Name for mongoose connection. If undefined, the default connection name will be used.
  */
 export interface KindagooseModuleOptions extends ConnectOptions {
     connectionName?: string;
